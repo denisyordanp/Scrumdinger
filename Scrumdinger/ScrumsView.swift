13d48e1c5c5a75dc12f622303a821528c5804caf
Denis Yordan P <denis.yordan.p@gmail.com>
2024-08-24 16:14:12 +0700

    Displaying data in list

---
+++ Scrumdinger/ScrumsView.swift
@@ -0,0 +1,18 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by Denis Yordan Panggabean on 24/08/24.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScrumsView()
+}
