88ced63a47b6ea3bef80ccd9838dd8dba47f62cc
Denis Yordan P <denis.yordan.p@gmail.com>
2024-08-24 07:40:49 +0700

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Denis Yordan Panggabean on 24/08/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
