7db4e8dc753704ba0794d14e2e58be54509c0a75
Denis Yordan P <denis.yordan.p@gmail.com>
2024-08-24 10:52:37 +0700

    Creating a card view

---
+++ Scrumdinger/CardView.swift
@@ -0,0 +1,37 @@
+//
+//  CardView.swift
+//  Scrumdinger
+//
+//  Created by Denis Yordan Panggabean on 24/08/24.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    let scrum: DailyScrum
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(scrum.title)
+                .font(.headline)
+                .accessibilityAddTraits(/*@START_MENU_TOKEN@*/.isHeader/*@END_MENU_TOKEN@*/)
+            Spacer()
+            HStack {
+                Label("\(scrum.attendees.count)", systemImage: "person.3")
+                    .accessibilityLabel("\(scrum.attendees.count) attendees")
+                Spacer()
+                Label("\(scrum.lengthInMinutes)", systemImage: "clock")
+                    .labelStyle(.trailingIcon)
+                    .accessibilityLabel("\(scrum.lengthInMinutes) minute meeting")
+            }
+            .font(.caption)
+        }
+        .padding()
+        .foregroundColor(scrum.theme.accentColor)
+    }
+}
+
+#Preview(traits: .fixedLayout(width: 400, height: 60)) {
+    let scrum = DailyScrum.sampleData[0]
+    return CardView(scrum: scrum)
+        .background(scrum.theme.mainColor)
+}
---
+++ Scrumdinger/Models/Theme.swift
@@ -0,0 +1,38 @@
+//
+//  Theme.swift
+//  Scrumdinger
+//
+//  Created by Denis Yordan Panggabean on 24/08/24.
+//
+
+import SwiftUI
+
+enum Theme : String {
+    case local_bubblegum
+    case local_buttercup
+    case local_indigo
+    case local_lavender
+    case local_magenta
+    case local_navy
+    case local_orange
+    case local_oxblood
+    case local_periwinkle
+    case local_poppy
+    case local_purple
+    case local_seafoam
+    case local_sky
+    case local_tan
+    case local_teal
+    case local_yellow
+    
+    var accentColor: Color {
+        switch self {
+        case .local_bubblegum, .local_buttercup, .local_lavender, .local_orange, .local_periwinkle, .local_poppy, .local_seafoam, .local_sky, .local_tan, .local_teal, .local_yellow: return .black
+        case .local_indigo, .local_magenta, .local_navy, .local_oxblood, .local_purple: return .white
+        }
+    }
+    
+    var mainColor: Color {
+        Color(rawValue)
+    }
+}
